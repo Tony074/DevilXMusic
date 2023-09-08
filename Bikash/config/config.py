9b136d2b35e5cbf393a6cded772ef4a1d856b612
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-09-08 18:16:51 +0530

    Update config.py

--- Bikash/config/config.py
+++ Bikash/config/config.py
@@ -15,15 +15,15 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "༺ 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 ༻")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1439222689").split()))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "༺ 𝐌𝐨𝐧𝐮 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 ༻")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6402115032").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BikashHalderNew/Bgtplayer")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Tony074/DevilXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "bikash")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BikashGadgetsTech")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Bgt_Chat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/About_Info_Devil")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/mission_successs")
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "180"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
@@ -68,20 +68,20 @@ clean = {}
 autoclean = []
 ############################
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/3127b3484676bd2254e97.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/f6be70d55d8c5c5fe0e3b.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/f6be70d55d8c5c5fe0e3b.jpg")
 
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg"
-GLOBAL_IMG_URL = "https://te.legra.ph/file/2e2741f5dfe9f62eed91d.png"
-STATS_IMG_URL = "https://te.legra.ph/file/2be94fc76030833b51a78.png"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/d68f9ff85714d4dbc6069.png"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/d68f9ff85714d4dbc6069.png"
-STREAM_IMG_URL = "https://graph.org/file/93882ae5ea01a7bf687b1.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/0d021735560cbf0bb749a.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/4a8cc770a5bea2136bada.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+GLOBAL_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/34de57f748fccecebacc3.jpg"
 
 
 def time_to_seconds(time):
@@ -107,9 +107,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "resources/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/3127b3484676bd2254e97.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/f6be70d55d8c5c5fe0e3b.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "resources/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg"
+            START_IMG_URL = "https://telegra.ph/file/f6be70d55d8c5c5fe0e3b.jpg"
